19f0e2e936b19795f3cef149e29fa51b1e0c012e
Martin Grajeda Jr <martingrajedajr@gmail.com>
2022-05-17 20:14:44 -0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -2,13 +2,13 @@ function NavBar(){
   return(
     <>
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
+      <div className="container-fluid">
       <a className="navbar-brand" href="#">BadBank</a>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarScroll">
-        <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
+        <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
           <li className="nav-item">
             <a className="nav-link" aria-current="page" href="#/CreateAccount/">Create Account</a>
           </li>
